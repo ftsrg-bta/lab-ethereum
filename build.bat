3ce3eb99eb890e588f264a01945eca1a1815fb94
bp99 <bertalan.peter@bp99.eu>
2024-04-18 17:10:47 +0200

    Upgrade workflows

--- build.bat
+++
@@ -1,7 +0,0 @@
-:: HTML
-pandoc.exe guide.md --from=markdown_github-hard_line_breaks+yaml_metadata_block --output=guide.html --css=pandoc.css --standalone --self-contained --syntax-definition solidity.xml
-pandoc.exe vulnerabilities.md --from=markdown_github-hard_line_breaks+yaml_metadata_block --output=vulnerabilities.html --css=pandoc.css --standalone --self-contained
-
-:: PDF
-pandoc.exe guide.md --from=markdown_github-hard_line_breaks+yaml_metadata_block --output=guide.pdf -V geometry:margin=1in -V urlcolor:blue --syntax-definition solidity.xml --highlight-style breezedark
-pandoc.exe vulnerabilities.md --from=markdown_github-hard_line_breaks+yaml_metadata_block --output=vulnerabilities.pdf -V geometry:margin=1in -V urlcolor:blue
